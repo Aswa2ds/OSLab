eb9bb50a86406ceb5cc440810fdc170e7cdb4bef
edwardmartin927 <edwardmartin927@gmail.com>
2018-05-22 22:47:22 +0800

    update pcb create ProcessTable struct

--- lab3/kernel/include/x86/pcb.h
+++ lab3/kernel/include/x86/pcb.h
@@ -1,4 +1,20 @@
 #ifndef __X86_PCB_H__
 #define __X86_PCB_H__
 
+#define MAX_STACK_SIZE 16 << 8
+#define MAX_PCB_NUM 20
+
+struct ProcessTable {
+    uint32_t stack[MAX_STACK_SIZE]; // 内核堆栈
+    struct TrapFrame tf;
+    int state;
+    int timeCount;
+    int sleepTime;
+    uint32_t pid;
+    struct ProcessTable *next;
+};
+
+struct ProcessTable pcb[MAX_PCB_NUM];
+
+
 #endif
